55dbdb13c722cae6676c336a04a6ee5476b39b10
Денис Усов <Denis.usov@ukr.net>
2022-09-28 21:50:10 +0300

    add uuid

--- controllers/auth/register.js
+++ controllers/auth/register.js
@@ -1,6 +1,9 @@
 const {basedir} = global
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const { v4: uuidv4 } = require('uuid')
 const User = require(`${basedir}/models/user`)
+const {SECRET_KEY} = process.env
 
 const register = async (req, res) => {
     const {email, password} = req.body
@@ -9,7 +12,11 @@ const register = async (req, res) => {
         return res.status(409).json({ status: 'error', code: 409, message: `User with ${email} is already exist!`})
     }
     const hashPassword = await bcrypt.hash(password, 10)
-    const user = await User.create({...req.body, password: hashPassword})
+    const payload = {
+        id: uuidv4()
+    }
+    const token = jwt.sign(payload, SECRET_KEY, {expiresIn: "1h"})
+    const user = await User.create({...req.body, password: hashPassword, token})
     res.status(201).json({ status: 'success', code: 201, data: {name: user.name, email: user.email}})
 }
 
